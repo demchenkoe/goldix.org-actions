40e0a7699ce191390a56648e7443cf90ff128bc2
Demchenko Evgeny <demchenko@inf.ai>
2018-04-27 10:01:45 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const {Action} = require('./lib/Action');
+const {Controller} = require('./lib/Controller');
+const {Validator, CompositeType, Type} = require('@goldix.org/types');
+
+
+module.exports = {Action, Controller, Validator, CompositeType, Type };
